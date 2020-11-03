52d073c3d7b0dfb5cf8060941d3badd20efe3e7b
Valentina Chitic <valentina.chriss@hotmail.com>
2020-11-03 14:58:04 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chitic_Valentina_lab5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    enum ActionState 
+    {
+        New,
+        Edit,
+        Delete,
+        Nothing
+    }
+
+    public partial class MainWindow : Window
+    {
+        ActionState action = ActionState.Nothing;
+        PhoneNumbersDataSet phoneNumbersDataSet = new PhoneNumbersDataSet();
+        PhoneNumbersDataSetTableAdapters.PhoneNumbersTableAdapter tblPhoneNumbersAdapter = new PhoneNumbersDataSetTableAdapters.PhoneNumbersTableAdapter();
+        Binding txtPhoneNumberBinding = new Binding();
+        Binding txtSubcriberBinding = new Binding();
+        Binding txtContractValueBinding = new Binding();
+        Binding txtContractDateBinding = new Binding();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            grdMain.DataContext = phoneNumbersDataSet.PhoneNumbers;
+            txtPhoneNumberBinding.Path = new PropertyPath("Phonenum");
+            txtSubcriberBinding.Path = new PropertyPath("Subscriber");
+            txtContractValueBinding.Path = new PropertyPath("Contract_Value");
+            txtContractDateBinding.Path = new PropertyPath("Contract_Date");
+            txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+            txtSubscriber.SetBinding(TextBox.TextProperty, txtSubcriberBinding);
+            txtContractValue.SetBinding(TextBox.TextProperty, txtContractValueBinding);
+            txtContractDate.SetBinding(TextBox.TextProperty, txtContractDateBinding);
+        }
+
+        private void lstPhonesLoad()
+        {
+            tblPhoneNumbersAdapter.Fill(phoneNumbersDataSet.PhoneNumbers);
+        }
+
+        private void grdMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            lstPhonesLoad();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            if(MessageBox.Show("Close Application?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            Chitic_Valentina_lab5.PhoneNumbersDataSet phoneNumbersDataSet = ((Chitic_Valentina_lab5.PhoneNumbersDataSet)(this.FindResource("phoneNumbersDataSet")));
+            System.Windows.Data.CollectionViewSource phoneNumbersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("phoneNumbersViewSource")));
+            phoneNumbersViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.New;
+            btnNew.IsEnabled = false;
+            btnEdit.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+
+            btnSave.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+            lstPhones.IsEnabled = false;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+            txtPhoneNumber.IsEnabled = true;
+            txtSubscriber.IsEnabled = true;
+            txtContractValue.IsEnabled = true;
+            txtContractDate.IsEnabled = true;
+
+            BindingOperations.ClearBinding(txtPhoneNumber, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtSubscriber, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtContractValue, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtContractDate, TextBox.TextProperty);
+            txtPhoneNumber.Text = "";
+            txtSubscriber.Text = "";
+            txtContractValue.Text = "";
+            txtContractDate.Text = "";
+            Keyboard.Focus(txtPhoneNumber);
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Edit;
+            string tempPhonenum = txtPhoneNumber.Text.ToString();
+            string tempSubscriber = txtSubscriber.Text.ToString();
+            string tempContractValue = txtContractValue.Text.ToString();
+            string tempContractDate = txtContractDate.Text.ToString();
+
+            btnNew.IsEnabled = false;
+            btnEdit.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+
+            btnSave.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+            lstPhones.IsEnabled = false;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+            txtPhoneNumber.IsEnabled = true;
+            txtSubscriber.IsEnabled = true;
+            txtContractValue.IsEnabled = true;
+            txtContractDate.IsEnabled = true;
+
+            BindingOperations.ClearBinding(txtPhoneNumber, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtSubscriber, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtContractValue, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtContractDate, TextBox.TextProperty);
+            txtPhoneNumber.Text = tempPhonenum;
+            txtSubscriber.Text = tempSubscriber;
+            txtContractValue.Text = tempContractValue;
+            txtContractDate.Text = tempContractDate;
+            Keyboard.Focus(txtPhoneNumber);
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Delete;
+            string tempPhonenum = txtPhoneNumber.Text.ToString();
+            string tempSubscriber = txtSubscriber.Text.ToString();
+            string tempContractValue = txtContractValue.Text.ToString();
+            string tempContractDate = txtContractDate.Text.ToString();
+
+            btnNew.IsEnabled = false;
+            btnEdit.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+
+            btnSave.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+            lstPhones.IsEnabled = false;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+
+            BindingOperations.ClearBinding(txtPhoneNumber, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtSubscriber, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtContractValue, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtContractDate, TextBox.TextProperty);
+            txtPhoneNumber.Text = tempPhonenum;
+            txtSubscriber.Text = tempSubscriber;
+            txtContractValue.Text = tempContractValue;
+            txtContractDate.Text = tempContractDate;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Nothing;
+
+            btnNew.IsEnabled = true;
+            btnEdit.IsEnabled = true;
+            btnDelete.IsEnabled = true;
+            btnSave.IsEnabled = false;
+            btnCancel.IsEnabled = false;
+            lstPhones.IsEnabled = true;
+            btnPrevious.IsEnabled = true;
+            btnNext.IsEnabled = true;
+            txtPhoneNumber.IsEnabled = false;
+            txtSubscriber.IsEnabled = false;
+            txtContractValue.IsEnabled = false;
+            txtContractDate.IsEnabled = false;
+
+            txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+            txtSubscriber.SetBinding(TextBox.TextProperty, txtSubcriberBinding);
+            txtContractValue.SetBinding(TextBox.TextProperty, txtContractValueBinding);
+            txtContractDate.SetBinding(TextBox.TextProperty, txtContractDateBinding);
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if(action == ActionState.New)
+            {
+                try
+                {
+                    DataRow newRow = phoneNumbersDataSet.PhoneNumbers.NewRow();
+                    newRow.BeginEdit();
+                    newRow["Phonenum"] = txtPhoneNumber.Text.Trim();
+                    newRow["Subscriber"] = txtSubscriber.Text.Trim();
+                    newRow["Contract_Value"] = txtContractValue.Text.Trim();
+                    newRow["Contract_Date"] = txtContractDate.Text.Trim();
+                    newRow.EndEdit();
+                    phoneNumbersDataSet.PhoneNumbers.Rows.Add(newRow);
+                    tblPhoneNumbersAdapter.Update(phoneNumbersDataSet.PhoneNumbers);
+                    phoneNumbersDataSet.AcceptChanges();
+                }
+                catch (DataException ex)
+                {
+                    phoneNumbersDataSet.RejectChanges();
+                    MessageBox.Show(ex.Message);
+                }
+
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                lstPhones.IsEnabled = true;
+                btnPrevious.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                txtPhoneNumber.IsEnabled = false;
+                txtSubscriber.IsEnabled = false;
+                txtContractValue.IsEnabled = false;
+                txtContractDate.IsEnabled = false;
+            }
+            else
+                if(action == ActionState.Edit)
+            {
+                try
+                {
+                    DataRow editRow = phoneNumbersDataSet.PhoneNumbers.Rows[lstPhones.SelectedIndex];
+                    editRow.BeginEdit();
+                    editRow["Phonenum"] = txtPhoneNumber.Text.Trim();
+                    editRow["Subscriber"] = txtSubscriber.Text.Trim();
+                    editRow["Contract_Value"] = txtContractValue.Text.Trim();
+                    editRow["Contract_Date"] = txtContractDate.Text.Trim();
+                    editRow.EndEdit();
+                    tblPhoneNumbersAdapter.Update(phoneNumbersDataSet.PhoneNumbers);
+                    phoneNumbersDataSet.AcceptChanges();
+                }
+                catch (DataException ex)
+                {
+                    phoneNumbersDataSet.RejectChanges();
+                    MessageBox.Show(ex.Message);
+                }
+
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnDelete.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                lstPhones.IsEnabled = true;
+                btnPrevious.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                txtPhoneNumber.IsEnabled = false;
+                txtSubscriber.IsEnabled = false;
+                txtContractValue.IsEnabled = false;
+                txtContractDate.IsEnabled = false;
+
+                txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+                txtSubscriber.SetBinding(TextBox.TextProperty, txtSubcriberBinding);
+                txtContractValue.SetBinding(TextBox.TextProperty, txtContractValueBinding);
+                txtContractDate.SetBinding(TextBox.TextProperty, txtContractDateBinding);
+            }
+            else
+                if(action == ActionState.Delete)
+            {
+                try
+                {
+                    DataRow deleterow = phoneNumbersDataSet.PhoneNumbers.Rows[lstPhones.SelectedIndex];
+                    deleterow.Delete();
+                    tblPhoneNumbersAdapter.Update(phoneNumbersDataSet.PhoneNumbers);
+                    phoneNumbersDataSet.AcceptChanges();
+                }
+                catch (DataException ex)
+                {
+                    phoneNumbersDataSet.RejectChanges();
+                    MessageBox.Show(ex.Message);
+                }
+
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnDelete.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                lstPhones.IsEnabled = true;
+                btnPrevious.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                txtPhoneNumber.IsEnabled = false;
+                txtSubscriber.IsEnabled = false;
+                txtContractValue.IsEnabled = false;
+                txtContractDate.IsEnabled = false;
+
+                txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+                txtSubscriber.SetBinding(TextBox.TextProperty, txtSubcriberBinding);
+                txtContractValue.SetBinding(TextBox.TextProperty, txtContractValueBinding);
+                txtContractDate.SetBinding(TextBox.TextProperty, txtContractDateBinding);
+            }
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            ICollectionView navigationView = CollectionViewSource.GetDefaultView(phoneNumbersDataSet.PhoneNumbers);
+            navigationView.MoveCurrentToPrevious();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            ICollectionView navigationView = CollectionViewSource.GetDefaultView(phoneNumbersDataSet.PhoneNumbers);
+            navigationView.MoveCurrentToNext();
+        }
+    }
+}
